2abe7febfa560ecc562f0ac7dacd8952aa231124
Vladica Stevanovic <vladica995@gmail.com>
2022-08-21 01:09:10 +0200

    Update za d15. i d16.

--- src/d16_08_2022/Proizvod.java
+++ src/d16_08_2022/Proizvod.java
@@ -4,15 +4,15 @@ public class Proizvod {
 //	Napisati klasu Proizvod koja ima atribute
 //	naziv proizvoda (String)
 //	cenu proizvoda (double)
-//	težinu proizvoda u gramima. (double)
+//	teï¿½inu proizvoda u gramima. (double)
 //		i metode:
 //	stampaj - stampa podatke proizvoda u formatu {{naziv}}, {{cena}}, {{tezina}}
-//	konvertuj - metoda konvertuje tezinu u kilograme i tone. Metoda kao parametar prima jedinicu u koju zelimo da konvertujemo, a metoda vraca sracunatu vrednost. Jedinica moze biti “kg” ili “t”
+//	konvertuj - metoda konvertuje tezinu u kilograme i tone. Metoda kao parametar prima jedinicu u koju zelimo da konvertujemo, a metoda vraca sracunatu vrednost. Jedinica moze biti ï¿½kgï¿½ ili ï¿½tï¿½
 //	Primer: ako proizvod ima 1200 grama i pozovemo metodu sa parametrom kg, metoda vraca 1.200
 //	Primer: ako proizvod ima 12000 grama i pozovemo metodu sa parametrom t, metoda vraca 0.012
-	String naziv;
-	double cena;
-	double tezina;
+	public String naziv;
+	public double cena;
+	 public double tezina;
 
 	public void  print() {
 	System.out.println("{" +naziv + "}" + " , " + "{" + cena + "}" + " , " + "{" + tezina + "}");
